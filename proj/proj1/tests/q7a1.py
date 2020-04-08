60a985a3b01342786e1e87cdc9608773ec416705
Yvonne Zhou <yvyfz@YvonnedeMacBook-Pro.local>
2020-04-08 23:19:51 +0800

    data100-fa19 successfully finished

---
+++ proj/proj1/tests/q7a1.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q7a1',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> max_swing in '
+                                               "set(bus['name'])\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
